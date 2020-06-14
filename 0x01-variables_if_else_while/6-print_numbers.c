5b12134f65c0ad37eb4bbcba102191c7728b3963
Justin4587 <Thurman@atlasok.com>
2020-06-13 20:29:05 -0500

    0123456789

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - 0 through 9
+* description: simple numerical output
+* Return: you know its 0 betty damn
+*/
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+while (n <= 9)
+{
+{
+putchar('0' + n);
+}
+if (n == 9)
+{
+putchar('\n');
+}
+n = n + 1;
+}
+return (0);
+}
