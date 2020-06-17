a68cd4f57e00cac627dce7c3e634854f6e96a024
Justin4587 <Thurman@atlasok.com>
2020-06-16 21:57:07 -0500

    second

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
-* print_sign - + - or 0 
+* print_sign - + - or 0
 * description: determines positive or negative
 * @n: int
 * Return: 0 1 or -1
@@ -10,7 +10,7 @@
 int print_sign(int n)
 {
 	int n;
-	
+
 if (n > 0)
 {
 _putchar("+");
