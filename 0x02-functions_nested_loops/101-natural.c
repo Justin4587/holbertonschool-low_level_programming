415cf3e0688e3afe7468cfd5e3191ef5584b7a9a
Justin4587 <Thurman@atlasok.com>
2020-07-03 22:51:13 -0500

    stuff

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,16 +9,15 @@
 
 int main(void)
 {
-	int sum, n;
+int sum, n;
 
-	sum = 0;
-	
-	for (n = 0; n < 1024; n++)
-	{
-		if (n % 3 == 0 || n % 5 == 0)
-		   sum = sum + n;
-		   }
-		   printf("%d\n", sum);
-		   return (0);
+sum = 0;
+
+for (n = 0; n < 1024; n++)
+{
+if (n % 3 == 0 || n % 5 == 0)
+sum = sum + n;
 }
-		   
+printf("%d\n", sum);
+return (0);
+}	   
