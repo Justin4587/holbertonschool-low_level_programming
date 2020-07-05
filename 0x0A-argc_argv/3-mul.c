c57ae7e5df68bc78baa35b25845f5032d137a2b8
Justin4587 <Thurman@atlasok.com>
2020-07-05 00:07:24 -0500

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,21 +11,21 @@
 
 int main(int argc, char *argv[])
 {
-  int a;
-  int b;
-  int c;
+int a;
+int b;
+int c;
 
-  if(argc != 3)
-    {
-    printf ("Error\n");
-    return (1);
-    }
-  else
-    {
-      b = atoi(argv[1]);
-  c = atoi(argv[2]);
-    a = (b * c);
-    printf("%d\n", a);
-    }
+if(argc != 3)
+{
+printf ("Error\n");
+return (1);
+}
+else
+{
+b = atoi(argv[1]);
+c = atoi(argv[2]);
+a = (b * c);
+printf("%d\n", a);
+}
 return (0);
 }
