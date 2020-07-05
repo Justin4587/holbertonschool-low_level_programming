553b19128cea8419ff41baf032fd2e9125084c74
Justin4587 <Thurman@atlasok.com>
2020-07-04 21:43:37 -0500

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* add - produces sum 
+* description - adds to ints 
+* @a: int
+* @b: int
+* Return: sum
+*/
+
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
