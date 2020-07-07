dc10ffabaa7e7b3664594a7bca64b9ca66d88de4
Justin4587 <Thurman@atlasok.com>
2020-07-07 15:36:57 -0500

    array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+* *create_array - init array
+* description: create array or error
+* @size: size of array
+* @c: char
+* Return: value or error
+*/
+
+char *create_array(unsigned int size, char c)
+{
+
+char *p;
+
+if (size >= 1)
+{
+p = malloc(sizeof(c) * size);
+if (p == NULL)
+return (NULL);
+}
+while (size != 0)
+{
+p[size - 1] = c;
+size--;
+return (p);
+}
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,4 @@
+
+
+void _putchar(char c);
+char *create_array(unsigned int size, char c);
