e60be3b808578097de5332e5d25d62901f9da749
Justin4587 <Thurman@atlasok.com>
2020-07-08 16:17:53 -0500

    strdup

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,44 +1,42 @@
 #include "holberton.h"
 
 /**
-* *str_concat - str array
+* str_concat - str array
 * description: append array
 * @s1: str to append
 * @s2: str to append
-* Return: value or error
+* Return: value of error
 */
+
 
 char *str_concat(char *s1, char *s2)
 {
-
+int count1 = 0;
+int count2 = 0;
+int i;
+int j;
 char *dup;
-int counter1 = 0;
-int counter2 = 0;
-int cnt = 0;
 
-while (s1[counter1] != '\0')
-counter1++;
-while (s2[counter2] != '\0')
-counter2++;
-if (s1 == NULL || s2 == NULL)
-return (NULL);
-{
-cnt = counter2 + counter1;
-}
-if (cnt >= 0)
-dup = malloc(sizeof(char) * (counter1 + counter2 + 1));
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+
+while (s1[count1] != '\0')
+count1++;
+while (s2[count2] != '\0')
+count2++;
+
+dup = malloc(sizeof(char) * (count1 + count2 + 1));
+
 if (dup == NULL)
 return (NULL);
-while (counter2 >= 0)
-{
-cnt--;
-counter2--;
-dup[cnt] = s2[counter2];
-}
-while (counter1 >= 0)
-{
-counter1--;
-dup[counter1] = s1[counter1];
-}
+
+for (i = 0; i < count1 ; i++)
+dup[i] = s1[i];
+
+for (j = 0; j <= count2 ; j++)
+dup[j + i] = s2[j];
+
 return (dup);
 }
