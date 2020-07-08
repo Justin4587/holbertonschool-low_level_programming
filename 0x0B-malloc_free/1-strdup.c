85b87e7b55b9640eaf5c8c674dee7ad8bbcf6992
Justin4587 <Thurman@atlasok.com>
2020-07-07 23:58:32 -0500

    dup2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,17 +13,20 @@ char *_strdup(char *str)
 char *dup;
 int counter = 0;
 
+if (str == NULL)
+return (NULL);
+
 while (str[counter] != '\0')
 counter++;
-
-
 
 if (counter >= 1)
 dup = malloc((sizeof(char)) * counter);
 if (dup == NULL)
 return (NULL);
 while (counter != 0)
-counter--;
+{
 dup[counter] = str[counter];
+counter--;
+}
 return (dup);
 }
