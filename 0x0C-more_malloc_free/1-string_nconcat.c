9982c0239501a147dbd0c948d002bcd803265ae2
Justin4587 <Thurman@atlasok.com>
2020-07-09 16:04:24 -0500

    asdf

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,20 +20,20 @@ while (s1[count1] != '\0')
 count1++;
 while (s2[count2] != '\0')
 count2++;
-if (count2 <= n - 1)
+if (count2 <= n)
 dup = malloc((sizeof(char)) * (count1 + count2 + 1));
-if (count2 > n - 1)
+if (count2 > n)
 dup = malloc((sizeof(char)) * (count1 + n + 1));
 if (dup == NULL)
 return (NULL);
 for (i = 0; i < count1 ; i++)
 dup[i] = s1[i];
-if (count2 < n - 1)
+if (count2 < n)
 {
 for (j = 0; j <= count2 ; j++)
 dup[j + i] = s2[j];
 }
-if (count2 >= n - 1)
+if (count2 >= n)
 for (j = 0; j <= n - 1 ; j++)
 dup[j + i] = s2[j];
 return (dup);
