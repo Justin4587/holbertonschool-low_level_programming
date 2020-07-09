3103c9d9cf439c2625e1e40016fae34b10d282e3
Justin4587 <Thurman@atlasok.com>
2020-07-08 23:52:54 -0500

    free

--- 0x0B-malloc_free/3-allo_grid.c
+++ 0x0B-malloc_free/3-allo_grid.c
@@ -16,14 +16,24 @@ int wide;
 
 if (width <= 0 || height <= 0)
 return (NULL);
+
 grid =(int **)malloc(sizeof(int *) * height);
 if(grid == NULL)
+{
+free(grid);
 return (NULL);
+}
+
 for (tall = 0; tall < height; tall++)
 {
 grid[tall] =(int *)malloc(sizeof(int) * width);
-if(grid == NULL)
+if(grid[tall] == NULL)
+{
+for (; tall >= 0; tall--)
+free(grid[tall]);
+free(grid);
 return (NULL);
+}   
 for (wide = 0; wide < width; wide++)
 grid[tall][wide] = 0;
 }
