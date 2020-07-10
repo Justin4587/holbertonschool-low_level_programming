1e6789cffd8106a8eb716ac524a9585b4544c397
Justin4587 <Thurman@atlasok.com>
2020-07-09 23:59:20 -0500

    asd

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,26 +11,24 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int count1 = 0, count2 = 0;
+unsigned int count1, count2;
 char *dup;
 
-if (s1 != NULL)
-while (s1[count1] != '\0')
-count1++;
-dup = malloc((sizeof(char)) * ((count1 + n) + 1));
-if (dup == NULL)
-{
-free(dup);
-return (NULL);
-}
-if (s1 != NULL)
-for (count1 = 0; s1[count1] != '\0'; count1++)
-dup[count1] = '\0';
-if (s2 != NULL)
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (count1 = 0; s1[count1]; count1++)
+;
 for (count2 = 0; count2 < n; count2++)
-{dup[count1] = s2[count2];
-count1++;
-}
+;
+dup = malloc(sizeof(char) * count1 + count2 + 1);
+if (dup == NULL)
+return (NULL);
+for (count1 = 0; s1[count1]; count1++)
+dup[count1] = s1[count1];
+for (count2 = 0; count2 < n && s2[count2]; count2++, count1++)
+dup[count1] = s2[count2];
 dup[count1] = '\0';
 return (dup);
 }
