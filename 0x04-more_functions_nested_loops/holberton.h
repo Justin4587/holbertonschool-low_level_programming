39f42cd17336edaf7a30d6a17bdf8f05889debbe
Justin4587 <Thurman@atlasok.com>
2020-07-11 17:20:52 -0500

    ABC

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x,int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int find_sqrt(int n, int i);
+int find_prime(int n, int i);
+int find_palindrome(char *s, int start, int end);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangles(int size);
+void print_number(int n);
