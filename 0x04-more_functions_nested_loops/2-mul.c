23d53788cd98ae6afa2b7e66f2211902f27cbe37
Justin4587 <Thurman@atlasok.com>
2020-07-11 17:48:55 -0500

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+
+/**
+* mul - mul ints
+* description: mul two ints
+* @a: int
+* @b: int
+* Return: value
+*/
+
+int mul(int a, int b)
+{
+int sum;
+sum = 0;
+
+sum = a * b;
+
+return (sum);
+}
