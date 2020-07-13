c201ee4d669271f1ebf8e298482964a1e03614ee
Justin4587 <Thurman@atlasok.com>
2020-07-12 22:13:42 -0500

    swap

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x,int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+int find_sqrt(int n, int i);
+int find_prime(int n, int i);
+int find_palindrome(char *s, int start, int end);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangles(int size);
+void print_number(int n);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
