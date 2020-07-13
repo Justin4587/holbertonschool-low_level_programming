aa83dbfd4f2ca1ad96c8ef6843254fc24efa96fe
Justin4587 <Thurman@atlasok.com>
2020-07-13 16:52:16 -0500

    dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+
+/**
+* struct dog - print compile file
+* description: dog stuff
+* @name: name of pet
+* @age: age
+* @owner: name of owner
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+struct dog;
+
+dog.name = name;
+dog.age = age;
+dog.owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include "holberton.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /**
 * struct dog - print compile file
