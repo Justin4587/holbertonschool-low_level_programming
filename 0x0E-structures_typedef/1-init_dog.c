6f3827344e5675405f97cd07521c9963159115e9
Justin4587 <Thurman@atlasok.com>
2020-07-13 19:21:17 -0500

    again

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,12 @@
 #include "dog.h"
 
 /**
-* struct dog - print compile file
+* init_dog - print compile file
 * description: dog stuff
 * @name: name of pet
 * @age: age
 * @owner: name of owner
+* @d: pointer
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
