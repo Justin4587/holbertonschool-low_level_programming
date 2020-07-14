a51a31cac7df6f687e2d727693f9f48162e0a70a
Justin4587 <Thurman@atlasok.com>
2020-07-14 13:57:22 -0500

    doggo

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,28 +12,37 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  int n;
-  int o;
-  dog_t *d;
-  char *new_name;
-  char *new_owner;
+int n;
+int o;
+dog_t *d;
 
-  d = malloc(sizeof(dog_t));
-  if (d == NULL)
-    return (NULL);
 
-  for (n = 0; name[n]; n++);
-  new_name = malloc(sizeof(char) * n + 1);
-  if (new_name == NULL)
-    return (NULL);
+d = malloc(sizeof(dog_t));
+if (d == NULL)
+{
+return (NULL);
+}
+for (n = 0; name[n]; n++)
+d->name = malloc(sizeof(char) * n + 1);
+if (d->name == NULL)
+{
+return (NULL);
+}
+for (o = 0; owner[o]; o++)
+d->owner = malloc(sizeof(char) * o + 1);
+if (d->owner == NULL)
+{
+return (NULL);
+}
+for (n = 0; name[n]; n++)
+d->name[n] = name[n];
+d->name[n] = '\0';
 
-  for (o = 0; owner[o]; o++);
-  new_owner = malloc(sizeof(char) * o + 1);
-  if (new_owner == NULL)
-    return (NULL);
+for (o = 0; owner[o]; o++)
+d->owner[o] = owner[o];
+d->owner[o] = '\0';
 
-  new_name = name;
-  new_owner = owner;
-  
-  return (d);
+d->age = age;
+
+return (d);
 }
