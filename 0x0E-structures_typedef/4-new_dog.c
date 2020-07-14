cdbe05e1c1dde36f6cd245c8b47d18db267740d1
Justin4587 <Thurman@atlasok.com>
2020-07-13 23:52:32 -0500

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,14 +18,19 @@ dog_t *new_dog(char *name, float age, char *owner)
   char *new_name;
   char *new_owner;
 
+  d = malloc(sizeof(dog_t));
+  if (d == NULL)
+    return (NULL);
 
   for (n = 0; name[n]; n++);
   new_name = malloc(sizeof(char) * n + 1);
+  if (new_name == NULL)
+    return (NULL);
 
   for (o = 0; owner[o]; o++);
   new_owner = malloc(sizeof(char) * o + 1);
-
-  d = malloc(sizeof(dog_t));
+  if (new_owner == NULL)
+    return (NULL);
 
   new_name = name;
   new_owner = owner;
