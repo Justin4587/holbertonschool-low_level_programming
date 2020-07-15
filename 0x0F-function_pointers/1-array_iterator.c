a6d716f05510b5786077d9c9df14c39f87b49c12
Justin4587 <Thurman@atlasok.com>
2020-07-15 18:41:19 -0500

    fp

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-
+#include <stddef.h>
 /**
 * array_iterator - function
 * description: print num
@@ -12,6 +12,8 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned int i;
+ size_t *pnt = &size;
+
 
 if (array == NULL)
 {
@@ -21,6 +23,10 @@ if (action == NULL)
 {
 return;
 }
-for (i = 0; i <= size; i++)
+if (pnt == NULL)
+{
+return;
+}
+for (i = 0; i < size; i++)
 action(array[i]);
 }
