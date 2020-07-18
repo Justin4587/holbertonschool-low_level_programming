d38382b9e81ac3244c75112f581cfdbd599297f7
Justin4587 <Thurman@atlasok.com>
2020-07-18 14:03:53 -0500

    arghhh

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,21 +18,17 @@ unsigned int i = 0;
 int j;
 va_start(argpnt, n);
 
-if (n == 0 || separator[0] == '\0')
+if (separator[0] == '\0')
 {
 return;
 }
 
 while (i < n)
 {
-{
 j = va_arg(argpnt, int);
-if (j == '\0')
-break;
 printf("%d", j);
 if (i < n - 1)
 printf("%s", separator);
-}
 i++;
 }
 putchar('\n');
