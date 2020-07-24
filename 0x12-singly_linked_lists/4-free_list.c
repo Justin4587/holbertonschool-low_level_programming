c93dff8d8aba778cc7a3a304873ea788b99d2b78
Justin4587 <Thurman@atlasok.com>
2020-07-24 14:19:44 -0500

    list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* free_list - function
+* description: print
+* @head: lists
+* Return: void
+*/
+
+void free_list(list_t *head)
+{
+
+   while (head != NULL)
+     {
+      free(head->str);
+      free(head);
+      head = head->next;
+     }
+   return;
+}
