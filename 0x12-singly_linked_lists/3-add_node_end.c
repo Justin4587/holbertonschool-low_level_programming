5933d2619638d33c2a1c115189731f881271715f
Justin4587 <Thurman@atlasok.com>
2020-07-24 10:56:49 -0500

    list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -6,14 +6,43 @@
 /**
 * add_node_end - function
 * description: print
-* @h: lists
+* @head: lists
+* @str: string
 * Return: size
 */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-list_t temp;
+list_t *new_end, *tail;
+unsigned int i = 0;
 
-temp = malloc(sizeof(list_t));
+ new_end = malloc(sizeof(list_t));
+
+ if(new_end == NULL)
+   {
+     return(NULL);
+   }
+ new_end->str = strdup(str);
+
+ while(str[i] != '\0')
+   {
+     i++;
+   }
+ new_end->len = i;
+
+ new_end->next = NULL;
+
+ if (*head == NULL)
+   *head = new_end;
+
+ else
+   {
+   tail = *head;
+   while (tail->next != NULL)
+     {
+       tail = tail->next;
+     }
+   tail->next = new_end;
+   }
+return (new_end);
 }
-
