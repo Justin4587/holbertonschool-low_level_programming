12a3bce7d5cc41485b6de6a68652fb36262fd777
Justin4587 <Thurman@atlasok.com>
2020-07-23 22:23:26 -0500

    list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* add_node - function
+* description: print
+* @h: lists
+* Return: size
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+list_t temp;
+
+temp = malloc(sizeof(list_t));
+temp = strdup(list_t->str);
+list_t->next;
+return (&temp);
+}
