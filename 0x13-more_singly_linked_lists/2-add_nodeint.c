f640831ce2156a0ff68bcf0e0bb818e4e29a51d3
Justin4587 <Thurman@atlasok.com>
2020-07-28 14:32:27 -0500

    first try 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* add_nodeint - function
+* description: print
+* @h: lists
+* Return: size
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *temp;
+
+temp = malloc(sizeof(listint_t));
+if(temp == NULL)
+{
+return(NULL);
+}
+temp->n = n;
+temp->next = *head;
+*head = temp;
+return (temp);
+}
