625ac67d37838a31decbf1c894341ed4217424ca
Justin4587 <Thurman@atlasok.com>
2020-07-28 21:48:14 -0500

    first try 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* free_listint2 - function
+* description: print
+* @head: lists
+* Return: void
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *pnt;
+listint_t *pntr;
+pnt = *head;
+
+
+while (pnt != NULL)
+{
+pntr = pnt->next;
+free(pnt);
+pnt = pntr;
+}
+*head = NULL;
+}
