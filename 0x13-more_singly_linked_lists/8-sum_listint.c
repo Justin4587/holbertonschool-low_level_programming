d67c3e837c82c3a86cce60acffbb64d50d54ab8a
Justin4587 <Thurman@atlasok.com>
2020-07-28 22:12:21 -0500

    first try 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* sum_listint - function
+* description: print
+* @head: lists
+* Return: size
+*/
+
+int sum_listint(listint_t *head)
+{
+int sum = 0;
+
+
+ while (head != NULL)
+   {
+     sum += head->n;
+     head = head->next;
+   }
+return (sum);
+}
