08067fca404d1f897ce8946b1a94d6c573458992
Justin4587 <Thurman@atlasok.com>
2020-07-30 19:09:38 -0500

    first try 0

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+unsigned int binary_to_uint(const char *b);
+
+
+
+#endif /* HOLBERTON_H */
