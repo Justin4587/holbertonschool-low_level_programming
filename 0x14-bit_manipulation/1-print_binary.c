d24c4c613afc57a4bf702c6d0d3f3ddb7ffa80c1
Justin4587 <Thurman@atlasok.com>
2020-07-30 21:44:37 -0500

    first try 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* print_binary - function
+* description: print
+* @n: lists
+* Return: size
+*/
+
+void print_binary(unsigned long int n)
+{
+
+ if (n == 0)
+   {
+     _putchar('0');
+     return;
+   }
+ else if (n == 1) 
+   {
+     _putchar('1');
+     return;
+   }
+
+ print_binary(n >> 1);
+ _putchar('0' + (n & 1));
+}
