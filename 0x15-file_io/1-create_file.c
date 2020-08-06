72ff250ca755f813e598da763277c6a903d12381
Justin4587 <Thurman@atlasok.com>
2020-08-06 14:42:07 -0500

    second try 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,36 +13,36 @@
 
 int create_file(const char *filename, char *text_content)
 {
-  int f;
-  int c;
-  int num = 0;
+int f;
+int c;
+int num = 0;
 
-  f = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+f = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
-  if (filename == NULL)
-    {
-      close(f);
-      return(-1);
-    }
+if (filename == NULL || f == -1)
+{
+close(f);
+return (-1);
+}
 
-  while(text_content[num])
-    {
-      num++;
-    }
+while (text_content[num])
+{
+num++;
+}
 
-  if (text_content == NULL)
-    {
-      close(f);
-      return(1);
-    }
+if (text_content == NULL)
+{
+close(f);
+return (1);
+}
 
-  c = write(f, text_content, num);
-  close(f);
-  if (c == -1)
-    {
-      return (-1);
-    }
-  return (1);
+c = write(f, text_content, num);
+close(f);
+if (c == -1)
+{
+return (-1);
+}
+return (1);
 }
 
 
