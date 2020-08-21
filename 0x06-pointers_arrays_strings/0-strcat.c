482acf8e6cbf81fe0d8061a84225ee551a769bfb
Justin4587 <Thurman@atlasok.com>
2020-08-21 15:02:24 -0500

    hello

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,21 +8,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *temp;
-	int n, i = 0, j = 0;
+	int i = 0, j = 0;
 
-	while (dest[i] != '\0')
-		i++;
+	i = _strlen(dest);
 
-	j = _strlen(src);
+	for (j = 0; src[j] != '\0'; i++, j++)
+		dest[i] = src[j];
+	dest[i] = '\0';
 
-	temp = malloc(sizeof(char) * (i + j) + 1);
-	for (n = 0; dest[n] != '\0'; n++)
-		temp[n] = dest[n];
-	for (j = 0; src[j] != '\0'; n++, j++)
-		temp[n] = src[j];
-	temp[n] = '\0';
-    dest = temp;
 	return (dest);
 }
 /**
