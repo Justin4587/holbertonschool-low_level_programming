02de851e2404bb090a5f56285a4162be40c8468b
Justin4587 <Thurman@atlasok.com>
2020-08-21 22:37:10 -0500

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * leet - compares two strings
+ * @n: string 2
+ * Return:n
+ */
+
+char *leet(char *n)
+{
+int i, j;
+char ptr[] = "aAeEoOtTlL";
+char str[] = "4433007711";
+
+for (i = 0; n[i] != '\0'; i++)
+{
+for(j = 0; j < 10; j++)
+{
+if (n[i] == ptr[j])
+n[i] = str[j];
+}
+}
+return (n);
+}
