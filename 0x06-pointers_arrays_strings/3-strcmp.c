8fc1470854a7b710f35d95ac68d7d425a37cc968
Justin4587 <Thurman@atlasok.com>
2020-08-21 20:42:22 -0500

    cmp 15

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,16 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-    int dif = 0;
+int dif = 0;
 
-	while (s1[i] != '\0')
-	{
+while (s1[i] != '\0')
+{
 if (s1[i] != s2[i])
 {
 dif = s1[i] - s2[i];
 return(dif);
 }
-		i++;
-	}
-	return (0);
+i++;
+}
+return (0);
 }
