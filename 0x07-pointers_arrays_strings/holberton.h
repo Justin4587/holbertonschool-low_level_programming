fc26e25367b52db13a42babb15fcc44cac25de50
Justin4587 <Thurman@atlasok.com>
2020-08-22 20:40:21 -0500

    strpbrk

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -7,6 +7,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
+char *_strpbrk(char *s, char *accept);
 
 
 #endif
