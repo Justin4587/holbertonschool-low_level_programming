d8e9730ddb67554427888cf0394875de8aecb64a
Justin4587 <Thurman@atlasok.com>
2020-08-22 23:38:22 -0500

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,35 +3,34 @@
 /**
 * _strstr - write value to buffer
 * description: take a value and write to buffer n times
-* @s: location being appended
-* @accept:char to look for
+* @haystack: location being appended
+* @needle:char to look for
 * Return: char str
 */
 
 char *_strstr(char *haystack, char *needle)
 {
-    int i = 0, j = 0, k;
+int i = 0, j = 0, k;
+k = _strlen(needle);
 
-    k = _strlen(needle);
-
-    while (haystack[i] != '\0')
-    {
-        if (haystack[i] == needle[j])
-        {
-            while (haystack[i] == needle[j])
-            {
-                if (j == k - 1)
-                {
-                    k = i - j;
-                    return ((char *) haystack + k);
-                }
-            j++;
-            i++;
-            }
-        }
-        i++;
-    }
-    return(0);
+while (haystack[i] != '\0')
+{
+if (haystack[i] == needle[j])
+{
+while (haystack[i] == needle[j])
+{
+if (j == k - 1)
+{
+k = i - j;
+return ((char *) haystack + k);
+}
+j++;
+i++;
+}
+}
+i++;
+}
+return (0);
 }
 
 
