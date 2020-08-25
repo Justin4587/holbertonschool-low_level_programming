b75e3063887690f5cf4912eb9f2c1065583e7837
Justin4587 <Thurman@atlasok.com>
2020-08-25 16:46:03 -0500

    cd

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,37 +13,31 @@
 
 int main(int ac, char **av)
 {
-  char buffer[1024];
-  int ff;
-  int ft;
-  int chars;
-  int abschar;
+char buffer[1024];
+int ff;
+int ft;
+int chars;
+int abschar;
 
-  if (ac != 3)
-    {
-      dprintf(STDOUT_FILENO, "Usage: cp file_from file_to/n");
-      return (97);
-    }
+if (ac != 3)
+{
+dprintf(STDOUT_FILENO, "Usage: cp file_from file_to/n");
+return (97);
+}
 
-  ff = open(av[1], O_RDONLY);
+ff = open(av[1], O_RDONLY);
 
-  ft = open(av[2], O_APPEND | O_CREAT | O_RDWR);
+ft = open(av[2], O_APPEND | O_CREAT | O_RDWR);
 
-  while ((chars = read(ff, buffer, 1024)) > 0)
-    {
-      if (chars == -1)
-	printf("trial");
-      abschar = write(ft, buffer, chars);
-      if (abschar == -1)
-	printf("trial");
-    }
+while ((chars = read(ff, buffer, 1024)) > 0)
+{
+if (chars == -1)
+abschar = write(ft, buffer, chars);
+if (abschar == -1)
+}
 
-  /**
-   *      dprintf(STDOUT_FILENO, "Error: Can't read from file %s", ac[1]);
-   *      return(0);
-   */
 
-  close(ff);
-  close(ft);
-  return (0);
+close(ff);
+close(ft);
+return (0);
 }
