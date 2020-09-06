5e27592cc89211d8bee7a91924200064028c0acc
Justin4587 <Thurman@atlasok.com>
2020-09-05 20:23:36 -0500

    add

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* add_dnodeint - function
+* description: print
+* @head: pointer
+* @n: int
+* Return: size
+*/
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *temp;
+
+if (head == NULL)
+return (NULL);
+
+temp = malloc(sizeof(dlistint_t));
+if (temp == NULL)
+{
+return (NULL);
+}
+temp->n = n;
+temp->next = *head;
+*head = temp;
+return (temp);
+}
