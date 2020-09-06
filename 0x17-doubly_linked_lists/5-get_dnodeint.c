1b17232b869433c5b86ba4fed27babc6de17c0d4
Justin4587 <Thurman@atlasok.com>
2020-09-06 12:33:57 -0500

    at_index

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* *get_dnodeint_at_index - function
+* description: print
+* @head: lists
+* @index: position
+* Return: size
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int counter = 0;
+
+while (counter < index)
+{
+head = head->next;
+counter++;
+}
+return (head);
+}
