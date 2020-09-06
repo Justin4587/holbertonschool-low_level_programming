2f8324ed877ed510ff9c14061a1712fa4717b9f6
Justin4587 <Thurman@atlasok.com>
2020-09-06 13:13:09 -0500

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 unsigned int i;
-dlistint_t *node, *temp;
+dlistint_t *node, *temp, *after_node;
 
 node = malloc(sizeof(dlistint_t));
 
@@ -26,12 +26,17 @@ if (node == NULL)
 return (NULL);
 for (i = 0; i < idx - 1; i++)
 {
-temp = temp->next;
-if (temp == NULL && i + 1 < idx)
-return (NULL);
+    temp = temp->next;
+    if (temp == NULL && i + 1 < idx)
+    return (NULL);
 }
-node->next = temp->next;
-temp->next = node;
+    after_node = temp->next;
+    temp->next = node;
+    node->prev = temp;
+    node->next = after_node;
+
+    
+    
 
 return (node);
 }
