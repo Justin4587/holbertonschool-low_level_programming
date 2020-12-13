8cc8e9dfa17c5f82cf298dae56f084b1cc184f12
Justin4587 <Thurman@atlasok.com>
2020-12-13 17:28:40 -0600

    get node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * *get_dnodeint_at_index - traverses linked list
+ * description: retrieve node
+ * @index: node to retrieve
+ * @head: head of list
+ * Return: none
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    unsigned int cnt = 0;
+
+    if (head == NULL)
+    return (NULL);
+
+    while (cnt < index)
+    {
+        head = head->next;
+        cnt++;
+    }
+    return (head);
+}
