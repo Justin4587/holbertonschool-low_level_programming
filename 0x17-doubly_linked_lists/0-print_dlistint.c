057f8ff12650777374735840d43a176bde75e8a1
Justin4587 <Thurman@atlasok.com>
2020-12-13 14:13:32 -0600

    print stuff

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - traverses linked list
+ * description: prints ints in list
+ * @h: list
+ * Return: size of list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    unsigned int i = 0;
+
+    if (h == NULL)
+        return (i);
+    
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        i++;
+    }
+    return (i);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a README for doubly linked lists using c which I hate now. 
