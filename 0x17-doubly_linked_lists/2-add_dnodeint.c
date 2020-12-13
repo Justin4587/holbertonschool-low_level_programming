cdd70901590c2ef37c4d5d91123a6f6edffa2f67
Justin4587 <Thurman@atlasok.com>
2020-12-13 15:24:00 -0600

    broken

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - traverses linked list
+ * description: prints ints in list
+ * @h: list
+ * @head: head of list
+ * Return: size of list
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new;
+
+if (head == NULL)
+return (NULL);
+
+new = malloc(sizeof(dlistint_t));
+
+if (new == NULL)
+return (NULL);
+
+new->n = n;
+new->next = *head;
+new->prev = NULL;
+
+if (*head != NULL)
+(*head)->prev = new;
+
+*head = new;
+return (new);
+}
