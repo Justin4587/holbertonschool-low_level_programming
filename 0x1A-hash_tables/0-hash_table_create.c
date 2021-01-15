5bc5fb5d3bfe1b62da44139756aa19f673271c2f
Justin4587 <Thurman@atlasok.com>
2021-01-15 15:57:19 -0600

    hash BROWNS

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - create hashbrowns
+ * @size: size of hashbrowns
+ * Return: pnt
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashb;
+	unsigned long int i = 0;
+
+	if (size <= 0)
+		return (NULL);
+
+	hashb = malloc(sizeof(hash_table_t));
+
+	if (hashb == NULL)
+		return (NULL);
+
+	hashb->size = size;
+
+	hashb->array = malloc(sizeof(hash_node_t) * size);
+
+	if (hashb->array == NULL)
+	{
+		free(hashb);
+		return (NULL);
+	}
+
+	while (i < size)
+	{
+		hashb->array[i] = NULL;
+		i = i + 1;
+	}
+
+	return (hashb);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+This is a README for hash tables in c. 
