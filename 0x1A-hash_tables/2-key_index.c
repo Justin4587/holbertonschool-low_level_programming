d299202091f9d43fe07c54b6e885e14cf87b479c
Justin4587 <Thurman@atlasok.com>
2021-01-15 17:52:18 -0600

    key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,8 +2,8 @@
 
 /**
  * key_index - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
+ * @key: string used to generate hash value
+ * @size: um size
  * Return: hash value
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
