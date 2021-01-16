450dffa463315f7f61cf74a8e22d2ed4e87c6c93
Justin4587 <Thurman@atlasok.com>
2021-01-15 20:28:43 -0600

    get

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+#include "string.h"
+/**
+ * hash_table_ - implementation of the djb2 algorithm
+ * @ht: string used to generate hash value
+ * @key: um size
+ *
+ * Return: 1 or 0
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp;
+	unsigned long int i;
+	const unsigned char *key_copy = (const unsigned char *)key;
+
+	if (ht == NULL || strlen(key) == 0)
+		return (NULL);
+	i = key_index(key_copy, ht->size);
+	temp = ht->array[i];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
+
+		temp = temp->next;
+	}
+	return (NULL);
+}
