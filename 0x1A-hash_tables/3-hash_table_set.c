d92398372d5af94dcb906c6f8a50929ab776d2bb
Justin4587 <Thurman@atlasok.com>
2021-01-22 22:10:38 -0600

    asdgf

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -33,13 +33,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->key = strdup(key);
 	if (!new->key)
 	{
-		free(new);
 		return (0);
 	}
 	new->value = strdup(value);
 	if (new->value == NULL)
 	{
-		free(new);
 		return (0);
 	}
 	new->next = ht->array[i];
